b9779891acdf1c022fe79b861e4c65168d4cc9b1
Gabriel L. C <102259679+GabrielLBl@users.noreply.github.com>
2023-03-02 21:12:43 -0300

    Add files via upload

---
+++ ProvaFinal/PNotas/PNotas/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.listNotas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(135, 164);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(285, 119);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // listNotas
+            // 
+            this.listNotas.FormattingEnabled = true;
+            this.listNotas.ItemHeight = 21;
+            this.listNotas.Location = new System.Drawing.Point(563, 41);
+            this.listNotas.Name = "listNotas";
+            this.listNotas.Size = new System.Drawing.Size(355, 445);
+            this.listNotas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 528);
+            this.Controls.Add(this.listNotas);
+            this.Controls.Add(this.btnVerificar);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "PNota";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox listNotas;
+    }
+}
+
---
+++ ProvaFinal/PNotas/PNotas/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            // Realizado por Gabriel Leite Cardoso e Guilherme Kohler, prova pratica LP2
+            double[,] notas = new double[4, 10];
+            int i;
+            int j;
+            string QRespondida;
+            string [] Questao = {"A", "A", "C", "D", "C", "D", "E", "D", "B", "C" };
+
+
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    QRespondida = Interaction.InputBox((i + 1).ToString() + "º Aluno, digite a alternativa respondida da " + (j + 1).ToString() + "º questão ", "Entrada de Dados");
+
+                    QRespondida = QRespondida.ToUpper();
+
+                    if (QRespondida == "A" || QRespondida == "B" || QRespondida == "C" || QRespondida == "D" || QRespondida == "E" || QRespondida == " ")
+                    {
+
+                        if (QRespondida == Questao[j])
+                            listNotas.Items.Add("O aluno: " + (i + 1).ToString() + " acertou questão: " + (j + 1).ToString() + " era: " + Questao[j] + " escolheu: " + QRespondida);
+                        else
+                            listNotas.Items.Add("O aluno: " + (i + 1).ToString() + " errou questão: " + (j + 1).ToString() + " era: " + Questao[j] + " escolheu: " + QRespondida);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Apenas digite A, B, C, D ou E");
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+}
